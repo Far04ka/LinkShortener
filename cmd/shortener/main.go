ecd430944805be66562f10ac955276bc36ba0b42
Andrey <deemen987@gmail.com>
2024-07-30 01:33:25 +0300

    4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func init() {
-	conf.Conf = &conf.Config{Addr: &conf.AddrField{Val: "localhost:8080"}, Finaladdr: &conf.FinalAddrField{}}
+	conf.Conf = &conf.Config{Addr: &conf.AddrField{Val: "localhost:8080"}, Finaladdr: &conf.FinalAddrField{Val: "http://localhost:8080/", ShortAddr: "/"}}
 	conf.CreateConfig()
 	storage.Storage.Lnks = make(map[string]string)
 }
